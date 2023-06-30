97bec199b18da787516f97e53a7c5707496b56a5
Pratik-Mahajan257 <79212698+Pratik-Mahajan257@users.noreply.github.com>
2023-06-30 21:31:58 +0530

    new building started

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
-import AnimeSearchScreen from './AnimeSearchScreen';
+
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <AnimeSearchScreen />
+    <View >
+  HELLO
     </View>
   );
 }
+ 
