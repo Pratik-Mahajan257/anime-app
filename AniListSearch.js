d8eecaf203b113578ad676db57c8aca21b419aca
Pratik-Mahajan257 <79212698+Pratik-Mahajan257@users.noreply.github.com>
2023-06-30 22:55:58 +0530

    fetched API successfully

---
+++ AniListSearch.js
@@ -0,0 +1,134 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, FlatList, Image, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+const AniListSearch = () => {
+    const navigation = useNavigation();
+  const [searchTerm, setSearchTerm] = useState('');
+  const [animeList, setAnimeList] = useState([]);
+
+  const searchAnime = async () => {
+    try {
+      const response = await fetch('https://graphql.anilist.co', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          query: `
+            query ($search: String) {
+              Page {
+                media(search: $search, type: ANIME) {
+                  id
+                  title {
+                    romaji
+                  }
+                  coverImage {
+                    medium
+                  }
+                  description
+                  startDate {
+                    year
+                    month
+                    day
+                  }
+                  # Add any other desired fields here
+                }
+              }
+            }
+          `,
+          variables: {
+            search: searchTerm,
+          },
+        }),
+      });
+      
+      const { data } = await response.json();
+      setAnimeList(data?.Page?.media || []);
+    } catch (error) {
+      console.error('Error searching anime:', error);
+    }
+  };
+
+  const renderAnimeItem = ({ item }) => {
+    const { id, title, coverImage, description, startDate } = item;
+
+
+  const handleAnimePress = (item) => {
+    if (navigation) {
+      navigation.navigate('AnimeDetails', { anime: item });
+
+      const { title, description, startDate } = item;
+      Alert.alert(
+        title?.romaji || 'Anime Details',
+        `Description: ${description || 'N/A'}\n\nStart Date: ${startDate?.year || 'N/A'}`,
+      );
+    }
+  };
+
+    return (
+      <TouchableOpacity style={styles.animeItem} onPress={handleAnimePress}>
+        <Image source={{ uri: coverImage?.medium }} style={styles.animePoster} />
+        <Text style={styles.animeTitle}>{title?.romaji}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.searchBar}
+        placeholder="Search anime"
+        value={searchTerm}
+        onChangeText={setSearchTerm}
+        onSubmitEditing={searchAnime}
+      />
+      <FlatList
+        data={animeList}
+        renderItem={renderAnimeItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    
+    padding: 16,
+    marginTop: 15,
+    backgroundColor: 'grey',
+  },
+  searchBar: {
+    height: 40,
+    borderColor: 'blue',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+  },
+ animeItem: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  marginBottom: 16,
+  justifyContent: 'center',
+  flexWrap: 'wrap',
+   
+},
+animePoster: {
+  width: '50%', 
+  height: '100%',
+  aspectRatio: 0.67,
+  marginBottom: 8,
+  resizeMode: 'contain',
+  borderColor: 'blue',
+    borderWidth: 1,
+},
+animeTitle: {
+  fontSize: 16,
+  fontWeight: 'bold',
+  textAlign: 'center', // Center the title within the item container
+  width: '100%', // Set the width to 100% to fill the item container
+}
+
+});
+
+export default AniListSearch;
--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import AniListSearch from './AniListSearch';
 
-
-export default function App() {
+const App = () => {
   return (
-    <View >
-  HELLO
-    </View>
+      <NavigationContainer>
+
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <AniListSearch  />
+      </View>
+    </SafeAreaView>
+      </NavigationContainer>
   );
-}
- 
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  content: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 16,
+  },
+});
+
+export default App;
